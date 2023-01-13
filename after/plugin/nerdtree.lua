ec6b2467c620511583d50c1d22b99ace2c6926f6
Shail Patel <shailpatel67@gmail.com>
2023-01-12 22:41:43 -0500

    more changes

---
+++ after/plugin/nerdtree.lua
@@ -0,0 +1,8 @@
+vim.keymap.set("n", "<C-B>", ":NERDTreeToggle<CR>")
+
+vim.cmd [[ 
+    autocmd VimEnter * NERDTree | wincmd p 
+    autocmd BufEnter * if tabpagenr('$') == 1 && winnr('$') == 1 && exists('b:NERDTree') && b:NERDTree.isTabTree() | quit | endif
+    autocmd BufEnter * if bufname('#') =~ 'NERD_tree_\d\+' && bufname('%') !~ 'NERD_tree_\d\+' && winnr('$') > 1 |
+    \ let buf=bufnr() | buffer# | execute "normal! \<C-W>w" | execute 'buffer'.buf | endif
+]]
