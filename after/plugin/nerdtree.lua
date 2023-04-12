0684581880bf9386c62a07776418300732b727bf
Shail Patel <shailpatel67@gmail.com>
2023-04-11 21:23:55 -0400

    save local config

--- after/plugin/nerdtree.lua
+++ after/plugin/nerdtree.lua
@@ -5,4 +5,6 @@ vim.cmd [[
     autocmd BufEnter * if tabpagenr('$') == 1 && winnr('$') == 1 && exists('b:NERDTree') && b:NERDTree.isTabTree() | quit | endif
     autocmd BufEnter * if bufname('#') =~ 'NERD_tree_\d\+' && bufname('%') !~ 'NERD_tree_\d\+' && winnr('$') > 1 |
     \ let buf=bufnr() | buffer# | execute "normal! \<C-W>w" | execute 'buffer'.buf | endif
+    let NERDTreeShowHidden=1
 ]]
+
