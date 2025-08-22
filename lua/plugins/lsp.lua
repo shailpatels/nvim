2f74e960cbf289378297c1c4de09ac550ac00503
Shail Patel <shailpatel67@gmail.com>
2025-08-21 21:43:01 -0400

    lsp issues

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,45 +1,54 @@
 return {
-	{
-		{
-			"williamboman/mason.nvim",
-			config = function()
-				require('mason').setup()
-			end
-		},
+    {
+        {
+            "williamboman/mason.nvim",
+            config = function()
+                require('mason').setup()
+            end
+        },
 
-		{
-			"williamboman/mason-lspconfig.nvim",
+        {
+            "williamboman/mason-lspconfig.nvim",
             dependencies = {
-                { "mason-org/mason.nvim", opts = {} },
-                "neovim/nvim-lspconfig",
+                "williamboman/mason.nvim"
             },
             opts = {
-                ensure_installed = { "pyright", "rust_analyzer", "zls", "intelephense" },
+                ensure_installed = { "pyright", "rust_analyzer", "zls", "intelephense", "clangd" },
                 automatic_enable = false,
             },
         },
 
-		{
-			"neovim/nvim-lspconfig",
-			config = function()
-				local lspconfig = require('lspconfig')
-				local on_attach = function(client, bufnr)
-					local bufopts = { noremap = true, silent = true, buffer = bufnr }
-					vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-					vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-					vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-					vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-					vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-					vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-					vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
-				end
+        {
+            "neovim/nvim-lspconfig",
+            config = function()
+                local lspconfig = require('lspconfig')
+                local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-				lspconfig.pyright.setup({ on_attach = on_attach })
-				lspconfig.tsserver.setup({ on_attach = on_attach })
-				lspconfig.intelephense.setup({ on_attach = on_attach })
+                local on_attach = function(client, bufnr)
+                    local bufopts = { noremap = true, silent = true, buffer = bufnr }
+                    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+                    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+                    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+                    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+                    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
 
-				lspconfig.rust_analyzer.setup({ 
+                    vim.keymap.set({'n','v'}, '<space>ca', function()
+                        vim.lsp.buf.code_action()
+                    end, bufopts)
+
+                    vim.keymap.set({'n','v'}, '<space>f', function()
+                        vim.lsp.buf.format({ async = true })
+                    end, bufopts)
+                end
+
+                lspconfig.pyright.setup({ on_attach = on_attach, capabilities = capabilities })
+                lspconfig.tsserver.setup({ on_attach = on_attach, capabilities = capabilities })
+                lspconfig.intelephense.setup({ on_attach = on_attach, capabilities = capabilities  })
+                lspconfig.clangd.setup({ on_attach = on_attach, capabilities = capabilities })
+
+                lspconfig.rust_analyzer.setup({ 
                     on_attach = on_attach,
+                    capabilities = capabilities,
                     settings = {
                         ["rust-analyzer"] = {
                             imports = {
@@ -60,8 +69,9 @@ return {
                     }
                 })
 
-				lspconfig.zls.setup({ 
+                lspconfig.zls.setup({ 
                     on_attach = on_attach,
+                    capabilities = capabilities,
                     settings = {
                         zls = {
                             zig_exe_path = (function()
@@ -78,39 +88,42 @@ return {
                         }
                     }
                 })
-			end,
-		},
+            end,
+        },
 
-		{
-			'hrsh7th/nvim-cmp',
-			config = function()
-				local cmp = require('cmp')
-				cmp.setup({
-					snippet = {
-						expand = function(args)
-							require('luasnip').lsp_expand(args.body)
-						end,
-					},
-					mapping = cmp.mapping.preset.insert({
-						['<C-b>'] = cmp.mapping.scroll_docs(-4),
-						['<C-f>'] = cmp.mapping.scroll_docs(4),
-						['<C-Space>'] = cmp.mapping.complete(),
-						['<C-e>'] = cmp.mapping.abort(),
-						['<CR>'] = cmp.mapping.confirm({ select = true }),
+        {
+            'hrsh7th/nvim-cmp',
+            dependencies = {
+                'hrsh7th/cmp-nvim-lsp',
+                'hrsh7th/cmp-buffer',     
+                'hrsh7th/cmp-path',      
+                'L3MON4D3/LuaSnip',
+            },
+            config = function()
+                local cmp = require('cmp')
+                cmp.setup({
+                    snippet = {
+                        expand = function(args)
+                            require('luasnip').lsp_expand(args.body)
+                        end,
+                    },
+                    mapping = cmp.mapping.preset.insert({
+                        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                        ['<C-Space>'] = cmp.mapping.complete(),
+                        ['<C-e>'] = cmp.mapping.abort(),
+                        ['<CR>'] = cmp.mapping.confirm({ select = true }),
                         ['<C-p>'] = cmp.mapping.select_prev_item(),
                         ['<C-n>'] = cmp.mapping.select_next_item(),
-					}),
-					sources = cmp.config.sources({
+                    }),
+                    sources = cmp.config.sources({
                         {name = 'nvim_lsp' },
                         {name = 'buffer' },
                         {name = 'path' },
-					})
-				})
-			end,
-		},
-
-		'hrsh7th/cmp-nvim-lsp',
-		'L3MON4D3/LuaSnip',
-	}
+                    })
+                })
+            end,
+        },
+    }
 
 }
